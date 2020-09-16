dbcb604a95c8741e9f004b77e25e2d48bca7723f
ishvar99 <ishanvarshney99@gmail.com>
2020-09-17 02:30:46 +0530

    added file sharing feature by tapping the user id

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 const app = express()
 const server = require("http").Server(app)
+const siofu = require("socketio-file-upload")
 const io = require("socket.io")(server)
 const { v4: uuidV4 } = require("uuid")
 app.set("view engine", "ejs")
+app.use(siofu.router)
 app.use(express.static("public"))
-
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`)
 })
@@ -23,6 +24,14 @@ io.on("connection", (socket) => {
       }
     })
     socket.to(roomId).emit("user-connected", socket.id)
+    var uploader = new siofu()
+    uploader.listen(socket)
+    uploader.on("error", function (event) {
+      console.log("Error from uploader", event)
+    })
+    socket.on("send-file", function (file, id, name) {
+      io.to(id).emit("recieve-file", file, name)
+    })
     socket.on("disconnect", () => {
       console.log("disconnected")
       socket.to(roomId).broadcast.emit("user-disconnected", socket.id)
--- public/script.js
+++ public/script.js
@@ -1,5 +1,8 @@
 let socket = io("/")
 let users = document.getElementById("users-list")
+let fileUpload = document.getElementById("siofu_input")
+let fName
+var targetSocketId
 socket.emit("join", roomId)
 socket.on("fetch-users", (clients, id) => {
   if (id === socket.id) {
@@ -21,9 +24,38 @@ socket.on("user-connected", (id) => {
 socket.on("user-disconnected", (id) => {
   removeUserFromList(id)
 })
-
+socket.on("recieve-file", (file, name) => {
+  const blob = new Blob([file])
+  const fileName = name
+  if (navigator.msSaveBlob) {
+    // IE 10+
+    navigator.msSaveBlob(blob, fileName)
+  } else {
+    const link = document.createElement("a")
+    // Browsers that support HTML5 download attribute
+    if (link.download !== undefined) {
+      const url = URL.createObjectURL(blob)
+      link.setAttribute("href", url)
+      link.setAttribute("download", fileName)
+      link.style.visibility = "hidden"
+      document.body.appendChild(link)
+      link.click()
+      document.body.removeChild(link)
+    }
+  }
+})
+fileUpload.addEventListener("change", (e) => {
+  fName = e.target.files[0].name
+})
+function uploadFile(e) {
+  console.log(e)
+  targetSocketId = e.target.textContent
+  fileUpload.click()
+}
 function addUserToList(userId, self) {
   let li = document.createElement("li")
+  li.onclick = (e) => uploadFile(e)
+  li.style.cursor = "pointer"
   li.appendChild(document.createTextNode(userId))
   if (self) {
     let span = document.createElement("span")
@@ -31,9 +63,24 @@ function addUserToList(userId, self) {
     strong.appendChild(document.createTextNode(" (You)"))
     span.appendChild(strong)
     li.appendChild(span)
+    li.style.cursor = "default"
+    li.onclick = null
   }
   users.appendChild(li)
 }
+
+let uploader = new SocketIOFileUpload(socket)
+uploader.listenOnInput(fileUpload)
+uploader.addEventListener("progress", function (event) {
+  console.log(event)
+  var percent = (event.bytesLoaded / event.file.size) * 100
+  console.log("File is", percent.toFixed(2), "percent loaded")
+})
+uploader.addEventListener("complete", function (event) {
+  if (event.success) {
+    socket.emit("send-file", event.file, targetSocketId, fName)
+  }
+})
 
 function removeUserFromList(id) {
   let userIds = users.getElementsByTagName("li")
