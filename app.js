430f83a796320c392e2db1bba39ce3f936366001
ishvar99 <ishanvarshney99@gmail.com>
2020-09-16 17:11:15 +0530

    using peerjs to handle multiple clients

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 const express = require("express")
-const socket = require("socket.io")
-let app = express()
-let PORT = process.env.PORT || 3000
+const app = express()
+const server = require("http").Server(app)
+const io = require("socket.io")(server)
+const { v4: uuidV4 } = require("uuid")
+
 app.set("view engine", "ejs")
 app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-  res.render("home")
+  res.redirect(`/${uuidV4()}`)
 })
-const server = app.listen(PORT, () => {
-  console.log("Server is running")
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room })
 })
-const io = socket(server)
+
 io.on("connection", (socket) => {
-  console.log("client connected")
+  console.log("client connected!")
+  socket.on("join", (roomId, userId) => {
+    socket.join(roomId) // join the room
+    socket.to(roomId).broadcast.emit("user-connected", userId)
+    socket.on("disconnect", () => {
+      console.log("disconnected")
+      socket.to(roomId).broadcast.emit("user-disconnected", userId)
+    })
+  })
+})
+server.listen(3000, () => {
+  console.log("Server is listening on PORT 3000")
 })
--- public/script.js
+++ public/script.js
@@ -1,7 +1,26 @@
-let socket = io()
-// socket.emit("connection", 20)
-
-let form = document.getElementById("form")
-form.addEventListener("submit", (e) => {
-  console.log(e)
+let socket = io("/")
+let users = document.getElementById("users-list")
+var peer = new Peer()
+peer.on("open", (userId) => {
+  populateUsersList(userId, " (You)")
+  socket.emit("join", roomId, userId)
 })
+socket.on("user-connected", (userId) => {
+  populateUsersList(userId)
+})
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close()
+})
+
+function populateUsersList(userId, tag) {
+  let li = document.createElement("li")
+  li.appendChild(document.createTextNode(userId))
+  if (tag) {
+    let span = document.createElement("span")
+    let strong = document.createElement("strong")
+    strong.appendChild(document.createTextNode(tag))
+    span.appendChild(strong)
+    li.appendChild(span)
+  }
+  users.appendChild(li)
+}
